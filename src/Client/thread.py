048f2ab4fa7340d40515a892bdb6f4ebeac6f5b0
Awy@bupt <wei.svip@foxmail.com>
2018-05-18 20:37:48 +0800

    code 1 update!

---
+++ src/Client/thread.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import threading
+import mainwindow
+
+t1=mainwindow.client("2015211302")
+class myThread (threading.Thread):
+    def __init__(self, counter):
+        threading.Thread.__init__(self)
+        self.counter = counter
+    def run(self):
+        if self.counter==1:
+           t1.termtask()
+        else:
+           t1.edittask()
+
+
+# 创建新线程
+thread1 = myThread(1)
+thread2 = myThread(2)
+
+# 开启新线程
+thread1.start()
+thread2.start()
+thread1.join()
+thread2.join()
+print ("退出主线程")
---
+++ src/Server/server.py
@@ -0,0 +1,22 @@
+import socket
+import json
+def conne(host="127.0.0.1",port=9008,maxlisten = 5):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen(maxlisten)
+        while True:
+            conn, addr = s.accept()
+            send_data = {'switch': 1, 'temperature': 25, 'wind': 1, 'cost': 2.00}
+            sendd = json.dumps(send_data)
+            conn.sendall(bytes(sendd, encoding="utf8"))
+            while True:
+                accept_data = str(conn.recv(1024),encoding="utf8")
+                print("".join(["接收报文", accept_data, "客户端口：", str(addr[1])]))
+                #if accept_data == "byebye":  # 如果接收到“byebye”则跳出循环结束和第一个客户端的通讯，开始与下一个客户端进行通讯
+                 #   break
+
+                break
+
+
+if __name__=="__main__":
+    conne()
